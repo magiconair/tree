0da03488f5084a554425a9eaac7b7fcc65b3e1cd
Frank Schroeder <frank.schroeder@gmail.com>
2017-11-26 23:09:22 +0100

    Make StringValue comparator less ambiguous.

--- binary/value.go
+++ binary/value.go
@@ -9,15 +9,15 @@ import (
 type Value interface {
 	// Compare returns an integer comparing two values.
 	// The result will be 0 if a==b, -1 if a < b, and +1 if a > b.
-	Compare(v Value) int
+	Compare(b Value) int
 }
 
 type StringValue string
 
-func (a StringValue) Compare(other Value) int {
-	b, ok := other.(StringValue)
+func (a StringValue) Compare(b Value) int {
+	bb, ok := b.(StringValue)
 	if !ok {
-		panic(fmt.Sprintf("not a string: %v", other))
+		panic(fmt.Sprintf("not a string: %v", b))
 	}
-	return strings.Compare(string(a), string(b))
+	return strings.Compare(string(a), string(bb))
 }
